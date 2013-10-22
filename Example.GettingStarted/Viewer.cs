48e3a0e4729bf89a1b5e3e91203a4b293b5de31b
David Hesselbom <david.hesselbom@imint.se>
2013-10-22 17:13:39 +0200

    Updated packages.

--- Example.GettingStarted/Viewer.cs
+++ Example.GettingStarted/Viewer.cs
@@ -12,26 +12,43 @@ namespace Example.GettingStarted
 		public Viewer()
 		{
 			this.InitializeComponent();
+			this.Load += (Object sender, EventArgs e) => Viewer_Load(sender, e);
 		}
 
 		void InitializeComponent()
 		{
-			this.vidview = new Imint.Vidview.Viewer();
 			this.SuspendLayout();
 			// 
-			// viewer
+			// Viewer
 			// 
-			this.vidview.Asynchronous = Settings.Asynchronous.SetNotify;
-			this.vidview.Arguments = "-r telnet://:23";
-			this.vidview.Dock = System.Windows.Forms.DockStyle.Fill;
-			this.vidview.Location = new System.Drawing.Point(0, 0);
-			this.vidview.Name = "viewer";
-			this.vidview.SeparateProcess = false;
-			this.vidview.Size = new System.Drawing.Size(800, 600);
-			this.vidview.TabIndex = 0;
-
-			this.Controls.Add(this.vidview);
+			this.Name = "Viewer";
+			this.Dock = Forms.DockStyle.Fill;
+			this.Size = new System.Drawing.Size(800, 600);
 			this.ResumeLayout(false);
+		}
+
+		protected virtual void Viewer_Load(Object sender, EventArgs e)
+		{
+			if (!DesignMode)
+			{
+				this.vidview = new Imint.Vidview.Viewer();
+				this.SuspendLayout();
+				// 
+				// viewer
+				// 
+				this.vidview.Asynchronous = Settings.Asynchronous.SetNotify;
+				this.vidview.Arguments = "-r telnet://:23";
+				this.vidview.Dock = System.Windows.Forms.DockStyle.Fill;
+				this.vidview.Location = new System.Drawing.Point(0, 0);
+				this.vidview.Name = "viewer";
+				// Running as a separate process requires the NuGet package "Imint.Vidview.Window". 
+				this.vidview.SeparateProcess = false;
+				this.vidview.Size = new System.Drawing.Size(800, 600);
+				this.vidview.TabIndex = 0;
+
+				this.Controls.Add(this.vidview);
+				this.ResumeLayout(false);
+			}
 		}
 	}
 }
